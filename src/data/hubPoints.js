e3af16bfb15aa98474570ea06ba9f1fd94ac35e3
fellypesyllva007 <53619427+fellypesyllva007@users.noreply.github.com>
2026-05-27 09:50:58 -0300

    feat(points): aggregate hub click points

---
+++ src/data/hubPoints.js
@@ -0,0 +1,31 @@
+import { inventoryPoint } from './points/inventory.js'
+import { mapsPoint } from './points/maps.js'
+import { dungeonPoint } from './points/dungeon.js'
+import { eventsPoint } from './points/events.js'
+import { profilePoint } from './points/profile.js'
+import { arenaPvpPoint } from './points/arenaPvp.js'
+import { commercePoint } from './points/commerce.js'
+import { riftsPoint } from './points/rifts.js'
+import { battlePoint } from './points/battle.js'
+import { guildPoint } from './points/guild.js'
+import { charactersPoint } from './points/characters.js'
+import { merchantPoint } from './points/merchant.js'
+import { gmPanelPoint } from './points/gmPanel.js'
+import { logoutPoint } from './points/logout.js'
+
+export const hubPoints = [
+  inventoryPoint,
+  mapsPoint,
+  dungeonPoint,
+  eventsPoint,
+  profilePoint,
+  arenaPvpPoint,
+  commercePoint,
+  riftsPoint,
+  battlePoint,
+  guildPoint,
+  charactersPoint,
+  merchantPoint,
+  gmPanelPoint,
+  logoutPoint,
+]
